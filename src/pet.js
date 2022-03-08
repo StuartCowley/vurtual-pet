b676c5357d49910343e225e352406fb08654b41f
Ben Quinn <benjamesquinn@gmail.com>
2022-03-08 22:26:52 +0000

    Completed Checkup step

--- src/pet.js
+++ src/pet.js
@@ -26,5 +26,15 @@ Pet.prototype.feed = function () {
         this.hunger -= 3;
     }
 }
+Pet.prototype.checkUp = function () {
+    if (this.fitness <= 3 && this.hunger >= 5) {
+        return `I am hungry AND I need a walk`
+    } else if (this.fitness <= 3) {
+        return `I need a walk`
+    } else if (this.hunger >= 5) {
+        return `I am hungry`
+    }
+}
+
 
 module.exports = Pet;
