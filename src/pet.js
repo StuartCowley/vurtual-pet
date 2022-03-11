308c2d6add897a2c00878a65fed4ca2c9974929b
Ben Quinn <benjamesquinn@gmail.com>
2022-03-11 17:08:23 +0000

    Added guard clauses

--- src/pet.js
+++ src/pet.js
@@ -13,11 +13,17 @@ Pet.prototype = {
     }
 };
 Pet.prototype.growUp = function () {
+    if (!this.isAlive) {
+        throw new Error('Your pet is no longer alive :(');
+    }
     this.age += 1;
     this.hunger += 5;
     this.fitness -= 3;
 }
 Pet.prototype.walk = function () {
+    if (!this.isAlive) {
+        throw new Error('Your pet is no longer alive :(');
+    }
     if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
         this.fitness += 4;
     } else {
@@ -25,6 +31,9 @@ Pet.prototype.walk = function () {
     }
 }
 Pet.prototype.feed = function () {
+    if (!this.isAlive) {
+        throw new Error('Your pet is no longer alive :(');
+    }
     if ((this.hunger - 3) < MINIMUM_HUNGER) {
         this.hunger = MINIMUM_HUNGER;
     } else {
