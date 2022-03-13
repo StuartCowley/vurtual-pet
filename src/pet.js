18002c7753e0abfc9d7c79b72d05bf3faf62e703
Ben Quinn <benjamesquinn@gmail.com>
2022-03-13 18:32:26 +0000

    Virtual-pet with class syntax

--- src/pet.js
+++ src/pet.js
@@ -9,17 +9,17 @@ const MAXIMUM_HUNGER = 10;
 const MINIMUM_HUNGER = 0; 
 const MAXIMUM_AGE = 30;
 
-function Pet(name) {
+class Pet {
+    constructor(name) {
     this.name = name;
     this.age = initialAge;
     this.hunger = initialHunger;
     this.fitness = initialFitness;
     this.children = [];
-};
+}
 
-Pet.prototype = {
-    // added a getter to the prototype to dynamically track the state of the pet
-    get isAlive() {
+    get isAlive () {
+        // added a getter to the prototype to dynamically track the state of the pet
       return (
           this.age < MAXIMUM_AGE 
           && 
@@ -27,61 +27,62 @@ Pet.prototype = {
           && 
           this.fitness > MINIMUM_FITNESS);git
     }
-};
 
-Pet.prototype.growUp = function () {
-    // age increases by 1, hunger increases by 5 and fitness dcreases by 3 when called
-    if (!this.isAlive) {
-        throw new Error('Your pet is no longer alive :(');
+    growUp () {
+        // age increases by 1, hunger increases by 5 and fitness dcreases by 3 when called
+        if (!this.isAlive) {
+            throw new Error('Your pet is no longer alive :(');
+        }
+        this.age += 1;
+        this.hunger += 5;
+        this.fitness -= 3;
     }
-    this.age += 1;
-    this.hunger += 5;
-    this.fitness -= 3;
-};
 
-Pet.prototype.walk = function () {
-    // fitness level increases by 4 when called
-    if (!this.isAlive) {
-        throw new Error('Your pet is no longer alive :(');
+    walk () {
+        // fitness level increases by 4 when called
+        if (!this.isAlive) {
+            throw new Error('Your pet is no longer alive :(');
+        }
+        if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
+            this.fitness += 4;
+        } else {
+            this.fitness = MAXIMUM_FITNESS;
+        }
     }
-    if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
-        this.fitness += 4;
-    } else {
-        this.fitness = MAXIMUM_FITNESS;
-    }
-};
 
-Pet.prototype.feed = function () {
-    // hunger level decreases by 3 when called
-    if (!this.isAlive) {
-        throw new Error('Your pet is no longer alive :(');
+    feed () {
+        // hunger level decreases by 3 when called
+        if (!this.isAlive) {
+            throw new Error('Your pet is no longer alive :(');
+        }
+        if ((this.hunger - 3) < MINIMUM_HUNGER) {
+            this.hunger = MINIMUM_HUNGER;
+        } else {
+            this.hunger -= 3;
+        }
     }
-    if ((this.hunger - 3) < MINIMUM_HUNGER) {
-        this.hunger = MINIMUM_HUNGER;
-    } else {
-        this.hunger -= 3;
+
+    checkUp () {
+        // returns string dependant on the current fitness and hunger levels of the pet when called
+        const hungry = this.hunger >= 5;
+        const unfit = this.fitness <= 3;
+
+        if (unfit && hungry) {
+            return `I am hungry AND I need a walk`
+        } else if (unfit) {
+            return `I need a walk`
+        } else if (hungry) {
+            return `I am hungry`
+        } else {
+            return `I feel great!`
+        }
     }
-};
 
-Pet.prototype.checkUp = function () {
-    // returns string dependant on the current fitness and hunger levels of the pet when called
-    const hungry = this.hunger >= 5;
-    const unfit = this.fitness <= 3;
-
-    if (unfit && hungry) {
-        return `I am hungry AND I need a walk`
-    } else if (unfit) {
-        return `I need a walk`
-    } else if (hungry) {
-        return `I am hungry`
-    } else {
-        return `I feel great!`
+    adoptChild (adoptedChild) {
+        // injects new child object into Pet instance
+        this.children.push(adoptedChild);
     }
-};
 
-Pet.prototype.adoptChild = function (adoptedChild) {
-    // injects new child object into Pet instance
-     this.children.push(adoptedChild);
 }
 
 module.exports = Pet;
